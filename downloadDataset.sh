60667a28d53885bdbe3eb39942a027228a5d4235
alexciru <alejandrocirugeda@gmail.com>
2024-04-11 15:31:44 +0200

    add the training lables in the script

--- downloadDataset.sh
+++ downloadDataset.sh
@@ -9,7 +9,13 @@ mkdir datasets/Kitti/training
 mv datasets/testing/image_2 datasets/Kitti/testing
 mv datasets/training/image_2 datasets/Kitti/training
 
+# labels 
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip
+unzip data_object_label_2.zip -d datasets/Kitti/training_labels/
+mv datasets/Kitti/training_labels/training/label_2 datasets/Kitti/training_labels/
+
 # clean
 rm -rf datasets/testing
 rm -rf datasets/trainig
 rm data_object_image_2.zip
+rm data_object_label_2.zip
